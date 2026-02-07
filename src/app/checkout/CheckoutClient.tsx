b47594308d8c218df08f713dfef74b181a284aba
Work <weare@forthecult.store>
2026-02-07 14:54:34 +0800

    next fix

--- src/app/checkout/CheckoutClient.tsx
+++ src/app/checkout/CheckoutClient.tsx
@@ -2991,12 +2991,12 @@ export function CheckoutClient() {
                                 className="size-4 border-input text-primary focus:ring-primary"
                               />
                               <span className="text-sm">{opt.label}</span>
-                              {USDC_LOGO_SRC[opt.value] ? (
+                              {USDC_LOGO_SRC[opt.value as keyof typeof USDC_LOGO_SRC] ? (
                                 <Image
                                   alt={opt.label}
                                   className="ml-auto h-7 w-9 shrink-0 object-contain"
                                   height={28}
-                                  src={USDC_LOGO_SRC[opt.value]!}
+                                  src={USDC_LOGO_SRC[opt.value as keyof typeof USDC_LOGO_SRC]!}
                                   width={36}
                                 />
                               ) : null}
@@ -3018,12 +3018,12 @@ export function CheckoutClient() {
                                 className="size-4 border-input text-primary focus:ring-primary"
                               />
                               <span className="text-sm">{opt.label}</span>
-                              {USDT_LOGO_SRC[opt.value] ? (
+                              {USDT_LOGO_SRC[opt.value as keyof typeof USDT_LOGO_SRC] ? (
                                 <Image
                                   alt={opt.label}
                                   className="ml-auto h-7 w-9 shrink-0 object-contain"
                                   height={28}
-                                  src={USDT_LOGO_SRC[opt.value]!}
+                                  src={USDT_LOGO_SRC[opt.value as keyof typeof USDT_LOGO_SRC]!}
                                   width={36}
                                 />
                               ) : null}
